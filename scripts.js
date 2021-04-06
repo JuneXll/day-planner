de7c8b825658768a7f721034e58b93736a950484
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-04-06 13:42:05 -0400

    Updated save button

--- scripts.js
+++ scripts.js
@@ -17,50 +17,6 @@ var currentDay = $("#currentDay");
 var today = moment().format("dddd, MMMM Do")
 currentDay.text(today);
 
-// // Selected variables
-// var textArea = $("#text");
-// var startOfDay = moment("9", "h").format("hA");
-// var endOfDay = moment("17", "h").format("hA");
-// var now = moment().format("hA");
-// var save = $(".saveBtn");
-
-// // Function that turns the color of each timeblock by hour(Past,Present,Future)
-// // function checkTimeblock(){
-
-// //     for(var i=0;timeNum;i++){
-// //         var span = $("span");
-// //         var dataNum = span.attr("data-number");
-// //         var timeNum = moment(dataNum, "h").format("hA");
-
-// //         if(moment().isBetween(timeNum, now)){
-// //         textArea.addClass("past");
-// //         } else if(now){
-// //         textArea.addClass("present");
-// //         } else {
-// //         textArea.addClass("future");
-// //         }
-        
-// //         dataNum++;
-// //         break;
-// //     } 
-// // };
-
-// // checkTimeblock()
-// // console.log(timeNum);
-// // console.log(moment().isBetween(timeNum, now));
-
-// // Event to listen for the save button
-// save.on("click", saveEvent);
-// // Function that takes the value of the textArea and puts it in the local storage
-// function saveEvent(event){
-//     var event=$("#text").val();
-//     textArea.text(localStorage.getItem("event"));
-//     localStorage.setItem("event", (JSON.stringify(event)));
-
-// }
-
-
-
 $(document).ready(function () {
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
